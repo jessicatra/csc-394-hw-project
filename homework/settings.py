aa68bcb42e2ab6f5762164dac410cb1d5af2eac1
JESSICA TRAN <106770@lpc-depaulsecure-254-207.depaulsecure-student.depaul.edu>
2023-01-12 15:03:23 -0600

    Update settings.py

--- homework/settings.py
+++ homework/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-ty+=k#c1r$i%2oqs1dv*_2i^$sh=ruq1fp*n-etw%=3kd0qu8-
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
@@ -75,8 +75,12 @@ WSGI_APPLICATION = 'homework.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'postgres',
+        'USER': '106770',
+        'PASSWORD': 'Tra344233',
+        'HOST': '0.0.0.0',
+        'PORT': '5432',
     }
 }
 
